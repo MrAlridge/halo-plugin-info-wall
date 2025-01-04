76806100b31caf101b5cb148226fe967bdd9f9a1
FloatingHeart <rnloser@163.com>
2025-01-04 16:54:22 +0800

    修改了插件的一些信息

---
+++ src/main/java/run/f10atingheart/infoWall/InfoWallPlugin.java
@@ -0,0 +1,12 @@
+package run.f10atingheart.infoWall;
+
+import org.springframework.stereotype.Component;
+import run.halo.app.plugin.BasePlugin;
+import run.halo.app.plugin.PluginContext;
+
+@Component
+public class InfoWallPlugin extends BasePlugin {
+    public InfoWallPlugin(PluginContext pluginContext) {
+        super(pluginContext);
+    }
+}
